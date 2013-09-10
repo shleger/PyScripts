6302e85e698c0161b223a307a0ddcabe314811f8
Andrey Shleger <shleger@i-teco.ru>
2013-09-10 15:56:44 +0400

    num2words 1 to 1000

--- countNumberLetters.py
+++ countNumberLetters.py
@@ -2,5 +2,14 @@ __author__ = 'shleger'
 
 # from num2word.num2word_EN import n2w, to_card, to_ord, to_ordnum
 from  num2words.num2word_EN_GB import to_card
+import re
 
-print to_card(123)
+
+MAX_VAL = 1000
+sum = 0
+
+for i in range(1, MAX_VAL + 1):
+    s = re.sub('[\s,-]', '', to_card(i))
+    sum += s.__len__()
+    print i.__str__() + " " + sum.__str__()
+
