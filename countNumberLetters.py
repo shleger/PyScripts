e39375cc19a53a5c374f15bb1900d7a24c4f74a0
Andrey Shleger <shleger@i-teco.ru>
2013-09-10 15:34:47 +0400

    num2words to_card

--- countNumberLetters.py
+++ countNumberLetters.py
@@ -1,6 +1,6 @@
 __author__ = 'shleger'
 
 # from num2word.num2word_EN import n2w, to_card, to_ord, to_ordnum
-import  num2words.num2word_EN_GB
+from  num2words.num2word_EN_GB import to_card
 
-print num2words.num2word_EN_GB.to_card(123)
+print to_card(123)
