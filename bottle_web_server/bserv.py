b5c5b0cf506c703e834417a6fc5c56f9a72c26fb
Andrey Shleger <shleger@i-teco.ru>
2013-09-11 16:58:13 +0400

    bottle service

---
+++ bottle_web_server/bserv.py
@@ -0,0 +1,26 @@
+__author__ = 'pacopablo'
+
+from bottle import Bottle,route, run, template, request, post, get
+
+app = Bottle()
+
+@app.route('/')
+def hello():
+    return template('<b>Usage:<br><br> http://localhost:{{port}}/get <br> http://localhost:{{port}}/post</b>', port=8042)
+
+@app.route('/post', method='POST')
+def doPost():
+    return do_response()
+
+
+@app.route('/get', method='GET')
+def doPost():
+    return do_response()
+
+
+def do_response():
+    headers = ''
+    body = ''
+    for h in request.headers:
+        headers += h + ":" + request.headers.get(h) + "\n"
+    return headers + ''.join(request.body.readlines())
